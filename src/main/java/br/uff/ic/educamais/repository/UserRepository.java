c5eca32e2f8ec51a19128e37dca9704f1725bb43
Mathunes <matheusantunes720@gmail.com>
2021-12-13 15:46:13 -0300

    remove test classes

--- src/main/java/br/uff/ic/educamais/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.uff.ic.educamais.repository;
-
-import br.uff.ic.educamais.model.UserModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserModel, Integer> {
-
-}
