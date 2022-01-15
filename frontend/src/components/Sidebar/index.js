459c2bb831f7c242625fb78549110cd0f521ffa9
Mathunes <matheusantunes720@gmail.com>
2022-01-15 18:16:00 -0300

    add comfortaa font family

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './styles.css';
+import Logo from '../../assets/images/logo.png';
 
 function Sidebar() {
     return (
-        <div>
-           
-        </div>
+        <nav className='container'>
+           <div>
+                <img src={Logo} alt='logo'/>
+
+                <ul>
+                    <li>Autores</li>
+                    <li>Recursos</li>
+                    <li>Eventos</li>
+                    <li>Cursos</li>
+                </ul>
+           </div>
+        </nav>
     )
 }
 
