784bceab56e54e400a0719c7b4aafceba49d8a53
Mathunes <matheusantunes720@gmail.com>
2022-01-15 17:55:43 -0300

    remove semantic ui

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Sidebar from '../Sidebar';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      teste
+      <Sidebar />
     </div>
   );
 }
