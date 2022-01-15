1d3ce8aa9b6bea60f3c49f636319f62f919da645
Mathunes <matheusantunes720@gmail.com>
2022-01-15 18:49:09 -0300

    add menu button

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -2,42 +2,47 @@ import React from 'react';
 
 import './styles.css';
 import Logo from '../../assets/images/logo.png';
+import Menu from '../../assets/images/menu.png';
+
 import { Link } from 'react-router-dom';
 
 function Sidebar(props) {
     return (
-        <nav className='container'>
-           <div>
-                <Link to="/">
-                    <img src={Logo} alt='logo'/>
-                </Link>
-                <ul>
-                    <li>
-                        <Link to="/autores">
-                            Autores
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="recursos">
-                            Recursos
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="eventos">
-                            Eventos
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="cursos">
-                            Cursos
-                        </Link>
-                    </li>
-                </ul>
-           </div>
-           <div>
-               <p>Sobre</p>
-           </div>
-        </nav>
+        <>
+            <img src={Menu} alt='menu' id="menu" />
+            <nav className='container'>
+            <div>
+                    <Link to="/">
+                        <img src={Logo} alt='logo'/>
+                    </Link>
+                    <ul>
+                        <li>
+                            <Link to="/autores">
+                                Autores
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="recursos">
+                                Recursos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="eventos">
+                                Eventos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="cursos">
+                                Cursos
+                            </Link>
+                        </li>
+                    </ul>
+            </div>
+            <div>
+                <p>Sobre</p>
+            </div>
+            </nav>
+        </>
     )
 }
 
