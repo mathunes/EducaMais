933ccf8a4b6b23aeaf6063cfde58d34cef168cc8
Mathunes <matheusantunes720@gmail.com>
2022-01-17 11:44:28 -0300

    add resource details page

--- frontend/src/pages/ResourceDetails/index.js
+++ frontend/src/pages/ResourceDetails/index.js
@@ -1,10 +1,66 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 
+import ImageDefault from "../../assets/images/image-default.png";
+import Edit from "../../assets/images/edit.png";
+
+import "./styles.css";
+
 function ResourceDetails() {
     return (
         <div className="container-resource-details">
             <Sidebar />
+            <div className="resource-details">
+
+                <h1>Detalhes recurso</h1>
+                
+                <img src={ImageDefault} alt="representation" />
+
+                <div className="resource-details-content">
+
+                    <div className="resource-details-header">
+                        <h2>Título</h2>
+
+                        <div>
+                            <img src={Edit} alt="edit" />
+                            <a href="#">Acessar recurso</a>
+                        </div>
+                    </div>
+
+                    <div className="resource-details-keywords">
+                        <p>Palavra 1</p>
+                        <p>Palavra 2</p>
+                    </div>
+
+                    <p className="resource-description">Descrição lorem ipsum</p>
+
+                    <h2 className="authors">Autores</h2>
+
+                    <table>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Sobrenome</th>
+                            <th>Email</th>
+                            <th>Afiliação</th>
+                            <th>Orcid</th>
+                        </tr>
+                        <tr>
+                            <td>João</td>
+                            <td>Mendes</td>
+                            <td>joao@email.com</td>
+                            <td>afiliação</td>
+                            <td>123456</td>
+                        </tr>
+                    </table>
+                    
+                    <h2 className="collection">Coleção</h2>
+
+                    <p>Nome da coleção</p>
+
+                    <p className="dates">Criado em 12/12/2021 - Registrado em 12/12/2021</p>
+
+                </div>
+            </div>
         </div>
     )
 }
