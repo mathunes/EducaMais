5f2a2eb55607f69c9dfe64cdaf22e36026d77afc
Mathunes <matheusantunes720@gmail.com>
2022-01-17 21:14:21 -0300

    add function to handle keywords input

--- frontend/src/pages/NewResource/index.js
+++ frontend/src/pages/NewResource/index.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../../components/Sidebar";
 
 import "./styles.css";
 
 function NewResource() {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    const [keywords, setKeywords] = useState([]);
+    const [author, setAuthors] = useState("");
+    const [collection, setCollection] = useState("");
+
+    const encodeImageFileAsURL = (e) => {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.onloadend = () => {
+            setImage(reader.result);
+        }
+        reader.readAsDataURL(file);
+    }
+
+    const keyWordsStringToArray = (e) => {
+        var keyWordsString = e.target.value;
+
+        var keyWordsArray = keyWordsString.split(",");
+
+        var keyWordsArrayFormatted = keyWordsArray.map((keyWord) => {
+            return keyWord.trim();
+        });
+
+        setKeywords(keyWordsArrayFormatted);
+    }
+
+    const postData = (e) => {
+
+        e.preventDefault();
+
+        console.log(title);
+        console.log(description);
+        console.log(image);
+        console.log(keywords);
+        console.log(author);
+        console.log(collection);
+    }
+
     return (
         <div className="container-new-resource">
             <Sidebar />
@@ -12,18 +53,18 @@ function NewResource() {
 
                 <div className="new-resource-content">
 
-                    <form className="new-resource-form">
+                    <form className="new-resource-form" onSubmit={postData}>
                         <label for="title">Título</label>
-                        <input id="title" name="title"></input>
+                        <input id="title" name="title" onChange={(e) => setTitle(e.target.value)} />
                         
                         <label for="description">Descrição</label>
-                        <input id="description" name="description"></input>
+                        <input id="description" name="description" onChange={(e) => setDescription(e.target.value)} />
 
                         <label for="image">Imagem representativa</label>
-                        <input id="image" type="file" name="image"></input>
+                        <input id="image" type="file" name="image" onChange={encodeImageFileAsURL} />
 
                         <label for="keywords">Palavras chave</label>
-                        <input id="keywords" name="keywords"></input>
+                        <input id="keywords" name="keywords" onChange={keyWordsStringToArray} />
 
                         <label for="authors">Autor(es)</label>
                         <select id="authors" name="authors">
@@ -32,7 +73,7 @@ function NewResource() {
                         </select>
 
                         <label for="collection">Coleção</label>
-                        <select id="collection" name="collection">
+                        <select id="collection" name="collection" onChange={(e) => setCollection(e.target.value)}>
                             <option value="">Coleção 1</option>
                             <option value="">Coleção 2</option>
                         </select>
