0cd138a56f9b82ba56e45ef04b76409d0df213eb
Mathunes <matheusantunes720@gmail.com>
2022-01-20 22:43:37 -0300

    show course list

--- frontend/src/pages/Courses/index.js
+++ frontend/src/pages/Courses/index.js
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import Card from "../../components/Card";
+import Sidebar from "../../components/Sidebar";
+
+import "./styles.css";
 
 function Courses() {
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        axios.get(`https://educa-mais.herokuapp.com/course`)
+        .then((response) => {
+            setCourses(response.data);
+        })
+        .finally((response) => {
+            setLoading(false);
+        });
+
+    }, []);
+
+    let content;
+
+    if (loading) {
+
+        content = 
+            <div className="loading">
+                <p>Carregando...</p>
+            </div>
+
+    } else {
+
+        content = 
+                <div className="courses">
+                    <h1>Cursos</h1>
+                    <Link to="/curso/adicionar">Novo curso</Link>
+                    <div className="courses-list">
+                        {
+                            courses.map((data) => {
+                                return (
+                                    <Card 
+                                        key={data.id} 
+                                        id={data.id}
+                                        title={data.title}
+                                        image={data.image}
+                                        showOptions={true}
+                                        editLink={`/curso/editar?id=${data.id}`}
+                                        deleteLink={`/curso/excluir?id=${data.id}`}
+                                        showLink={`/curso/detalhes?id=${data.id}`}
+                                        entity="curso"
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+    }
+
     return (
-        <div>Courses</div>
+        <div className="container-course">
+            <Sidebar />
+            {content}
+        </div>
     )
 }
 
