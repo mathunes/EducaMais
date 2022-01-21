0e9e7621e99cdbeb87c9b021d17f708d3d9a3212
Mathunes <matheusantunes720@gmail.com>
2022-01-21 11:05:25 -0300

    add about modal

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -7,6 +7,13 @@ import Menu from '../../assets/images/menu.png';
 import { Link } from 'react-router-dom';
 
 function Sidebar(props) {
+
+    const [display, setDisplay] = useState("none");
+  
+    const handleClose = () => setDisplay("none");
+    const handleShow = () => setDisplay("flex");
+  
+
     return (
         <>
             {/* <img src={Menu} alt='menu' id="menu" /> */}
@@ -39,9 +46,21 @@ function Sidebar(props) {
                         </ul>
                 </div>
                 <div>
-                    <p>Sobre</p>
+                    <p className='btn-show-modal' onClick={handleShow}>Sobre</p>
                 </div>
             </nav>
+
+            <div className='modal-container' style={{display}} onClick={handleClose}>
+                <div className='modal'>
+                    <div className='modal-header'>
+                        <h2 className='modal-title'>Sobre</h2>
+                    </div>
+                    <div className='modal-body'>
+                        <p>Educa Mais é uma aplicação desenvolvida como requisito parcial para a conclusão da disciplina Desenvolvimento de Aplicações Corporativas - 2021.2, ministrada pelo professor José Viterbo Filho na Universidade Federal Fluminense.</p>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
